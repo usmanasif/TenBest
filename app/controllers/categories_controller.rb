5ef8b39a4628d595afe3e81b74241f4e3385129d
Zaid Annas <m.zaid.annas@devsinc.com>
2017-06-12 12:59:59 +0500

    Added tags in images alt and on ranking and place page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -70,7 +70,7 @@ class CategoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit( :name)
+      params.require(:category).permit( :name, :keywords)
     end
 
 end
