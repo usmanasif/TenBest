500fdc6a52799994168ad55fd75f916040e01282
Im-Genius <im.genius.man@gmail.com>
2017-07-14 13:09:10 +0500

    Tables style fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require dataTables/jquery.dataTables
 //= require turbolinks
 //= require_tree .
 //= require social-share-button
