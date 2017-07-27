e33ddd33b3e7bc6291ffcaa46cf3a21c9a117c96
Im-Genius <im.genius.man@gmail.com>
2017-07-27 18:12:28 +0500

    initail navlinks seeding added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616075750) do
+ActiveRecord::Schema.define(version: 20170726113846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,9 @@ ActiveRecord::Schema.define(version: 20170616075750) do
     t.integer  "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "parent_id"
+    t.string   "icon"
+    t.integer  "depth"
   end
 
   create_table "pronoun_orders", force: :cascade do |t|
