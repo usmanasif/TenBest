5add0177670861a49638f2a4df5e6b34c8843ba9
Im-Genius <im.genius.man@gmail.com>
2017-07-27 18:13:38 +0500

    Parent Child relationship added

--- app/models/nav_link.rb
+++ app/models/nav_link.rb
@@ -1,7 +1,9 @@
 class NavLink < ApplicationRecord
   validate :active_count
+  has_many :children, class_name: "NavLink", foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent, class_name: "NavLink"
 
   def active_count
-    NavLink.where(active: true).count >= 3 ? false : true
+    NavLink.where(active: true).count >= 1000 ? false : true
   end
 end
