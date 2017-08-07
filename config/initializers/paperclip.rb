554e7cebd84390e8db9aa6df00a4fc29c84288c7
Im-Genius <im.genius.man@gmail.com>
2017-08-07 12:23:47 +0500

    Moved paperclip per model prefrences to config/paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,15 +1,18 @@
 # for custom paperclip interpolates like record name that are not otherwise available in url
-Paperclip.interpolates('name') do |attachment, style|
-    attachment.instance.name.parameterize
+Paperclip.interpolates('name') do |attachment, _style|
+  attachment.instance.name.parameterize
 end
 Paperclip::Attachment.default_options.merge!(
   storage: :fog,
   fog_credentials: {
     provider: 'AWS',
-    aws_access_key_id: ENV["AMAZON_ID"],
-    aws_secret_access_key: ENV["AMAZON_KEY"],
-    :preserve_files => true,
-    region: ENV["REGION"],
+    aws_access_key_id: ENV['AMAZON_ID'],
+    aws_secret_access_key: ENV['AMAZON_KEY'],
+    preserve_files: false,
+    region: ENV['REGION']
   },
-  fog_directory: ENV["BUCKET"]
+  fog_directory: ENV['BUCKET'],
+  url: 'images/:class/:name/:style/:basename.:extension',
+  path: 'images/:class/:name/:style/:basename.:extension',
+  default_url: 'images/place-image.png'
 )
