e7ecd1b44f059f70dc567fa6c6ac2552ba20cf45
Im-Genius <im.genius.man@gmail.com>
2017-08-09 12:49:42 +0500

    NavLinks editing fixed

--- app/controllers/nav_links_controller.rb
+++ app/controllers/nav_links_controller.rb
@@ -6,7 +6,8 @@ class NavLinksController < ApplicationController
   # GET /nav_link
   # GET /nav_link.json
   def index
-    @nav_links = NavLink.all
+    @nav_links = NavLink.where(depth: 1).includes(:parent)
+    @parents = NavLink.where(depth: 0)
     @new_link = NavLink.new(parent_id: nil, depth: 1)
     flash[:new_alert] = 'not allowed any more active links' unless @new_link.active_count
   end
@@ -14,9 +15,9 @@ class NavLinksController < ApplicationController
   # GET /nav_link/1
   # GET /nav_link/1.json
   def show
-    @nav_links = NavLink.all
+    @nav_links = @nav_link.children
     @new_link = NavLink.new(parent_id: @nav_link.id, depth: 2)
-    flash[:new_alert] = 'not allowed any more active links' unless @new_link.active_count
+    flash.now[:new_alert] = 'not allowed any more active links' unless @new_link.active_count
   end
 
   # GET /nav_link/new
@@ -27,6 +28,7 @@ class NavLinksController < ApplicationController
   # GET /nav_link/1/edit
   def edit
     @nav_links = NavLink.all
+    @parents = NavLink.where(depth: 0)
     flash.now[:alert] = 'Cannot make this active, already have 5 active links' unless @nav_link.active && @nav_link.active_count
   end
 
@@ -34,11 +36,7 @@ class NavLinksController < ApplicationController
   # POST /nav_link.json
   def create
     @nav_link = NavLink.new(nav_link_params)
-    unless @nav_link.active
-      unless @nav_link.active_count
-        flash.now[:alert] = 'Cannot make this active, already have 5 active links'
-      end
-    end
+    flash.now[:alert] = 'Cannot make this active, already have 5 active links' unless @nav_link.active && @nav_link.active_count
     respond_to do |format|
       if @nav_link.save
         format.html { redirect_to nav_links_path, notice: 'nav_link was successfully created.' }
@@ -76,14 +74,15 @@ class NavLinksController < ApplicationController
 
   private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def nav_link_params
-      params.require(:nav_link).permit(:name, :url, :position, :active, :parent_id, :depth)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def nav_link_params
+    params.require(:nav_link).permit(:name, :url, :position, :active, :parent_id, :depth)
+  end
 
-    def set_layout
-      self.class.layout "admin"
-    end
+  def set_layout
+    self.class.layout 'admin'
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_nav_link
     @nav_link = NavLink.find(params[:id])
--- app/models/nav_link.rb
+++ app/models/nav_link.rb
@@ -4,6 +4,6 @@ class NavLink < ApplicationRecord
   belongs_to :parent, class_name: "NavLink"
 
   def active_count
-    NavLink.where(parent_id: self.parent_id, depth: self.depth, active: true).count < 5
+    NavLink.where(parent_id: parent_id, depth: depth, active: true).count < 5
   end
 end
